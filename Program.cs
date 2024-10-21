5c14271e7a95e350dbe1c0c83d13877fb7952089
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-21 11:19:54 -0300

    criado mais 2 produtos

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ using LojaVirtual.Service;
 List<IDesconto> produtos = new List<IDesconto>
 {
     new Eletronico("Play Station 9", 10000.00m),
-    new Roupa("Cuequinha Homem Aranha", 100.00m)
+    new Roupa("Cuequinha Homem Aranha", 100.00m),
+    new Eletronico("IPhone 16", 13000.90m),
+    new Roupa("Roupinha Bailarina Dinho", 500.00m)
+    
 };
 
 DescontoService descontoService = new DescontoService();
